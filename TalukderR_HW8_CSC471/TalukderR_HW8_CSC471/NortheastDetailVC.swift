38be1a841ee8487e2a093b67082eb009a1e53957
rtalukder <rtalukder@forsythe.com>
2020-03-16 22:15:25 -0500

    homework 8 completed

--- TalukderR_HW8_CSC471/TalukderR_HW8_CSC471/NortheastDetailVC.swift
+++ TalukderR_HW8_CSC471/TalukderR_HW8_CSC471/NortheastDetailVC.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class NortheastDetailVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var stateTitleLabel: UILabel!
+    @IBOutlet weak var stateFlagImage: UIImageView!
+    @IBOutlet weak var stateSubRegion: UILabel!
+    @IBOutlet weak var stateCapital: UILabel!
+    @IBOutlet weak var stateHoodDate: UILabel!
+    
+    var stateObject: NorthEastRegionStates?
+    
+    
+    @IBAction func cancelButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        if let state = stateObject {
+            stateTitleLabel.text = state.stateName
+            stateFlagImage.image = UIImage(named: state.stateImageFilePath)
+            stateSubRegion.text = state.stateSubRegion
+            stateCapital.text = state.stateCapital
+            stateHoodDate.text = state.stateHoodDate
+        }
+    }
 
     /*
     // MARK: - Navigation
